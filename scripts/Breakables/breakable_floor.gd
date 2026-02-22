4415faf0ff59720770a5739205ac2381ccc63507
TysonDanh <danhtyson22@gmail.com>
2026-02-21 23:27:04 -0700

    Added functions to wall, floor and rock. The wall and floor is now breakable and turn into rocks

---
+++ scripts/Breakables/breakable_floor.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var interactable: Area2D = $Interactable
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+@export var rock_scene: PackedScene
+@export var rock_count :=2
+
+
+func _ready() -> void:
+	interactable.interact = _on_interact
+	
+
+func _on_interact():
+	$Sprite2D.visible = false
+	$CollisionShape2D.disabled = true
+	
+	for i in rock_count:
+		var rock = rock_scene.instantiate()
+		rock.global_position = global_position + Vector2(randf_range(-16, 16), randf_range(-16, 16))
+		get_parent().add_child(rock)
+		
+		rock.apply_impulse(Vector2(randf_range(-100,100), randf_range(-200,-50)))
+		
+		queue_free()
