2bc3c388e68dc437778ac01de7295d066e304440
wpoon426 <wpoon426@mtroyal.ca>
2026-03-12 13:20:16 -0600

    Made the door in the tutorial room move to a another level

--- scripts/Interacts/lever.gd
+++ scripts/Interacts/lever.gd
@@ -2,20 +2,21 @@ extends Area2D
 
 @onready var interactable: Area2D = $Interactable
 @onready var animated_Sprite2D: AnimatedSprite2D = $AnimatedSprite2D
+@export var doors: NodePath
+
 
 var lever_on := false
 
-func _ready() -> void:
+	
+func _ready():
 	interactable.interact = _on_interact
 	
 func _on_interact():
-		toggle_level()
-		
-func toggle_level():
 	if lever_on:
 		return
 	lever_on = true
 	animated_Sprite2D.play("switch")
-	await animated_Sprite2D.animation_finished
-	animated_Sprite2D.play("on")
+	var door_node = get_node(doors)
+	if door_node:
+		door_node.open_door()
 	
