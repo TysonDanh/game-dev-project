0950f1791cb08f46d11a891b138bf18afb6707e6
Dylan S <dillymon.ds@gmail.com>
2026-03-27 19:10:40 -0600

    Level 1-1 assets added. Moving vine platforms added. Draft somewhat polished version of level 1-1 with collision, assets, and functionality working.

--- scripts/Interacts/button.gd
+++ scripts/Interacts/button.gd
@@ -1,23 +1,38 @@
 extends Area2D
 
 @onready var interactable: Area2D = $Interactable
-@onready var animated_Sprite2D: AnimatedSprite2D = $AnimatedSprite2D
-@export var doors: NodePath
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Parent stuff
+@export var parent_collision_shape_path: NodePath
+@export var parent_animated_sprite_path: NodePath
+@export var parent_animation_name: String = "Open"
+@export var disable_parent_collision: bool = true
 
 var pressed = false
 
 func _ready():
 	interactable.interact = _on_interact
-	
+
 func _on_interact():
 	if pressed:
 		return
+	
 	pressed = true
-	animated_Sprite2D.play("Pressed")
-	var door_node = get_node(doors)
-	if door_node:
-		door_node.open_door()
-		
-		
-
+	
+	# Keep button press animation
+	animated_sprite_2d.play("Pressed")
+	
+	# Toggle parent collision shape
+	if parent_collision_shape_path != NodePath():
+		var collision_shape = get_node_or_null(parent_collision_shape_path) as CollisionShape2D
+		if collision_shape:
+			collision_shape.disabled = disable_parent_collision
+	
+	# Play parent animated sprite animation
+	if parent_animated_sprite_path != NodePath():
+		var parent_sprite = get_node_or_null(parent_animated_sprite_path) as AnimatedSprite2D
+		if parent_sprite:
+			parent_sprite.play(parent_animation_name)
 	
